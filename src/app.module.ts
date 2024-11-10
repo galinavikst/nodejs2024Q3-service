2b69875aa8f119cbb7b4a23621964316e6921d08
galinavikst <galinavikst@gmail.com>
2024-11-10 20:54:26 +0200

    fix: lint warnings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TracksModule } from './tracks/tracks.module';
 import { AlbumsModule } from './albums/albums.module';
